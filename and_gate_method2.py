e1b2183ab6e8b1e5e3b329441733ea947d5c8579
https://github.com/Someshdiwan <someshdiwan369@gmail.com>
2024-12-31 09:19:11 +0000

    Add perceptron implementation Method 2

--- and_gate_method2.py
+++ and_gate_method2.py
@@ -1,28 +1,27 @@
 
-
 # perceptron implementation code 
 
 import numpy as np
 
 # define Unit Step Function
 def unitStep(v):
-	if v >= 0:
-		return 1
-	else:
-		return 0
+    if v >= 0:
+        return 1
+    else:
+        return 0
 
 # design Perceptron Model
 def perceptronModel(x, w, b):
-	v = np.dot(w, x) + b
-	y = unitStep(v)
-	return y
+    v = np.dot(w, x) + b
+    y = unitStep(v)
+    return y
 
 # AND Logic Function
 # w1 = 1, w2 = 1, b = -1.5
 def AND_logicFunction(x):
-	w = np.array([1, 1])
-	b = -1.5
-	return perceptronModel(x, w, b)
+    w = np.array([1, 1])
+    b = -1.5
+    return perceptronModel(x, w, b)
 
 # testing the Perceptron Model
 test1 = np.array([0, 1])
@@ -33,5 +32,7 @@ test4 = np.array([1, 0])
 print("AND({}, {}) = {}".format(0, 1, AND_logicFunction(test1)))
 print("AND({}, {}) = {}".format(1, 1, AND_logicFunction(test2)))
 print("AND({}, {}) = {}".format(0, 0, AND_logicFunction(test3)))
-print("AND({}, {}) = {}".format(1, 0, AND_logicFunction(test4)))      
-               
+print("AND({}, {}) = {}".format(1, 0, AND_logicFunction(test4)))
+
+//This implementation directly encodes the logic of the AND gate using a fixed set of weights and bias, which makes it simpler and doesn't require training.
+    
